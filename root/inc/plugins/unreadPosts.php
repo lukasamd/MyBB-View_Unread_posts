f2550fe74cf18145a50a3a4f1744c277690bbb66
Lukasz Tkacz <lukasz@tkacz.pro>
2019-01-22 08:37:43 +0100

    Version indication fix

--- root/inc/plugins/unreadPosts.php
+++ root/inc/plugins/unreadPosts.php
@@ -46,7 +46,7 @@ function unreadPosts_info() {
         'website' => 'https://tkacz.pro',
         'author' => 'Lukasz Tkacz',
         'authorsite' => 'https://tkacz.pro',
-        'version' => '1.11',
+        'version' => '1.12',
         'guid' => '',
         'compatibility' => '18*',
         'codename' => 'view_unread_posts',
